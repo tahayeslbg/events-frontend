66b23c28cff5b0c2e80f81712fb0362604e07c32
tahayeslbg <93354331+tahayeslbg@users.noreply.github.com>
2023-05-08 03:49:15 +0300

    asdasd

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-}
+	reactStrictMode: false,
+	swcMinify: true,
+	images: {
+		domains: ["res.cloudinary.com"],
+	},
+	env: {
+		API_URL: "http://164.92.228.40:3001",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? process.env.API_URL
+		? process.env.NEXT_PUBLIC_API_URL
 		: "http://localhost:3001";
 
 interface IUserRegister {
